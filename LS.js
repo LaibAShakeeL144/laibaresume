695591af09a35e5b50668bc3511c39357989387a
LaibAShakeeL144 <160258112+LaibAShakeeL144@users.noreply.github.com>
2024-09-08 17:18:35 +0500

    Add files via upload

---
+++ LS.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleSkillsBtn = document.getElementById('toggleSkillsBtn');
+    var skillsSection = document.querySelector('.skills');
+    if (toggleSkillsBtn) {
+        toggleSkillsBtn.addEventListener('click', function () {
+            if (skillsSection.style.display === 'none') {
+                skillsSection.style.display = 'block';
+                toggleSkillsBtn.textContent = 'Hide Skills';
+            }
+            else {
+                skillsSection.style.display = 'none';
+                toggleSkillsBtn.textContent = 'Show Skills';
+            }
+        });
+    }
+});
